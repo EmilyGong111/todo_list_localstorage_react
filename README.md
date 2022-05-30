229b8a78fa95b9c817ee5bb972c0b18432c54d32
Emily <mei.gong@icloud.com>
2022-05-30 16:01:22 +1000

    feat: useEffect async await fetch data and catch err from a restfulAPI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Todo List with add, search, checked, delete, localStorage functions
+
+
+- data - db.json -give some data
+- npx json-server -p 3500 -w data/db.json  -> start the data server, then you can fetch data from http://localhost:3500/items 
--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const APT_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null);
 
   useEffect(()=>{
-    localStorage.setItem('shoppinglist', JSON.stringify(items))
-  },[items])
+   const fetchItems = async () =>{
+     try{
+       const response = await fetch(APT_URL);
+       if(!response.ok) throw Error('Did not receive expected data')
+       const listItems = await response.json();
+       setItems(listItems);
+       setFetchError(null);
+     }catch(err){
+       setFetchError(err.message);
+     };
+   };
+   fetchItems();
+  },[])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -51,11 +65,16 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {fetchError && <p style = {{color: "red"}} >{`Error: ${fetchError}`}</p>}
+        {!fetchError && 
+          <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+          />
+        }
+       </main>
       <Footer length={items.length} />
     </div>
   );
